7ef8d34bc63d7c93f05ddf0f9e7c74c91a2589f4
alexraskin <alexraskin@fastmail.fm>
2024-05-09 14:13:39 -0700

    first commit

---
+++ Sober Since/Sober_SinceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sober_SinceApp.swift
+//  Sober Since
+//
+//  Created by Alexander Raskin on 5/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Sober_SinceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
